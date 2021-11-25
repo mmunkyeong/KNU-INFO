0b89dc2e22ac39e9f25ba7b97c0c06bbe26c65ad
mmunkyeong <62290451+mmunkyeong@users.noreply.github.com>
2021-11-25 23:51:44 +0900

    메인->채팅액티비티 이동

--- LEE/MyApplication/app/src/main/java/com/example/knu_info/MainActivity.java
+++ LEE/MyApplication/app/src/main/java/com/example/knu_info/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.knu_info;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            Button btnchat = (Button)findViewById(R.id.btnchat);
+
+            btnchat.setOnClickListener((view)->{
+                Intent in = new Intent(MainActivity.this,ChatActivity.class) ;
+                startActivity(in);
+            });
         }
+
 }
