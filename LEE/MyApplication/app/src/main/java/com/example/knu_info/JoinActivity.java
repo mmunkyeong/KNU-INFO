9a88d38e45af7b2e2365bfa1e20f5f9cad6f035a
mmunkyeong <62290451+mmunkyeong@users.noreply.github.com>
2021-11-30 23:56:53 +0900

    회원가입,로그인 기능 수정
    firestore 사용

--- LEE/MyApplication/app/src/main/java/com/example/knu_info/JoinActivity.java
+++ LEE/MyApplication/app/src/main/java/com/example/knu_info/JoinActivity.java
@@ -1,8 +1,5 @@
 package com.example.knu_info;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,18 +7,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class JoinActivity extends AppCompatActivity {
-    String TAG="JoinActivity";
-    EditText etjoinEmail;
+    String TAG = "JoinActivity";
+    EditText etJName;
+    EditText etJid;
     EditText etJpass;
+    EditText etJpass_check;
+    EditText etJEmail;
+    EditText etJPhone;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +41,87 @@ public class JoinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_join);
 
         mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener(){
-            public void onAuthStateChanged(FirebaseAuth firebaseAuth){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user != null){
-                    Log.d(TAG,"onAuthStateChanged:signed_in:"+user.getUid());
-                }
-                else{
-                    Log.d(TAG,"onAuthStateChanged:signed_out");
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
 
             }
         };
-        etjoinEmail = (EditText)findViewById(R.id.etjoinEmail);
-        etJpass=(EditText)findViewById(R.id.etJpass);
-        Button btnJjoin=(Button)findViewById(R.id.btnJjoin);
+        etJName = (EditText) findViewById(R.id.etJName);
+        etJid = (EditText) findViewById(R.id.etJid);
+        etJpass = (EditText) findViewById(R.id.etJpass);
+        etJpass_check = (EditText) findViewById(R.id.etJPasswordCheck);
+        etJPhone = (EditText) findViewById(R.id.etJPhone);
+        etJEmail = (EditText) findViewById(R.id.etJEmail);
 
+        Button btnJjoin = (Button) findViewById(R.id.btnJjoin);
 
+        //가입하기 버튼
+        btnJjoin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String stName = etJName.getText().toString();
+                String stId = etJid.getText().toString();
+                String stPass = etJpass.getText().toString();
+                String stPass_check = etJpass_check.getText().toString();
+                String stPhone = etJPhone.getText().toString();
+                String stEmail = etJEmail.getText().toString();
+                if (stPass.equals(stPass_check)) {
+                    HashMap<String, String> user = new HashMap<>();
+                    user.put("Name", stName);
+                    user.put("Id", stId);
+                    user.put("Password", stPass);
+                    user.put("Phone", stPhone);
+                    user.put("Email", stEmail);
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(getApplicationContext(), "가입이 완료되었습니다.", Toast.LENGTH_LONG).show();
+                                    finish();
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
 
-        btnJjoin.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                String stId = etjoinEmail.getText().toString();
-                String stPass=etJpass.getText().toString();
-                joinUser(stId,stPass);
+                } else {
+                    Toast.makeText(getApplicationContext(), "비밀번호가 서로 다릅니다.", Toast.LENGTH_LONG).show();
+                }
+
+                //joinUser(stId,stPass);
             }
         });
     }
-    public void onStart(){
+
+    public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
-    public void onStop(){
+
+    public void onStop() {
         super.onStop();
-        if(mAuthListener!=null) {
+        if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
-    public void joinUser(String id,String pass){
-        mAuth.createUserWithEmailAndPassword(id,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+    public void joinUser(String id, String pass) {
+        mAuth.createUserWithEmailAndPassword(id, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d(TAG,"createUserWithEmail:onComplete"+task.isSuccessful());
-                if(!task.isSuccessful()){
-                    Toast.makeText(JoinActivity.this,"Authentication failed.",Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "createUserWithEmail:onComplete" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Toast.makeText(JoinActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                 }
             }
 
--- LEE/MyApplication/app/src/main/java/com/example/knu_info/LoginActivity.java
+++ LEE/MyApplication/app/src/main/java/com/example/knu_info/LoginActivity.java
@@ -12,62 +12,87 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    EditText etID,etPassword;
-    String TAG="LoginActivity";
+    EditText etID, etPassword;
+    String TAG = "LoginActivity";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth=mAuth.getInstance();
+        mAuth = mAuth.getInstance();
 
-        Button btnJoin = (Button)findViewById(R.id.btnJoin);
-        Button btnLogin = (Button)findViewById(R.id.btnLogin);
-        etID=(EditText)findViewById(R.id.etID);
-        etPassword=(EditText)findViewById(R.id.etPassword);
+        Button btnJoin = (Button) findViewById(R.id.btnJoin);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        etID = (EditText) findViewById(R.id.etID);
+        etPassword = (EditText) findViewById(R.id.etPassword);
 
         //로그인 버튼 눌렀을 때
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 String id = etID.getText().toString().trim();
                 String pass = etPassword.getText().toString().trim();
-                LoginUser(id,pass);
+
+                db.collection("users")
+                        .whereEqualTo("Id", id)
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                    if (document.getData().get("Password").equals(pass)) {
+                                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(i);
+                                    } else {
+                                        Log.e(TAG, "onSuccess: 비밀번호가 일치하지 않음");
+                                        Toast.makeText(getApplicationContext(), "회원정보가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "onFailure: 로그인 실패");
+                            }
+                        });
+
+                //LoginUser(id,pass);
             }
         });
         //회원가입 버튼 눌렀을 때
-        btnJoin.setOnClickListener((view)->{
-            Intent in = new Intent(LoginActivity.this,JoinActivity.class) ;
+        btnJoin.setOnClickListener((view) ->
+
+        {
+            Intent in = new Intent(LoginActivity.this, JoinActivity.class);
             startActivity(in);
         });
 
     }
-    public void LoginUser(String id,String pass){
-        mAuth.signInWithEmailAndPassword(id,pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+
+    public void LoginUser(String id, String pass) {
+        mAuth.signInWithEmailAndPassword(id, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d(TAG,"signInWithEmail:onComplete"+task.isSuccessful());
-                if(task.isSuccessful()){
-                    Log.d(TAG,"signInWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    String stUserEmail=user.getEmail();
-                    String stUserName=user.getDisplayName();
-                    Log.d(TAG,"stUserEmail: "+stUserEmail+", stUserName: "+stUserName);
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                    intent.putExtra("email",id);
-
+                Log.d(TAG, "signInWithEmail:onComplete" + task.isSuccessful());
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
-
-                }
-                else{
-                    Log.w(TAG,"signInWithEmail",task.getException());
-                    Toast.makeText(LoginActivity.this,"로그인오류",Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.w(TAG, "signInWithEmail", task.getException());
+                    Toast.makeText(LoginActivity.this, "로그인오류", Toast.LENGTH_SHORT).show();
                 }
             }
         });
