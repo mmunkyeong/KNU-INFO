22541294243a52ec475369647f3d7905aeb12ac8
mmunkyeong <62290451+mmunkyeong@users.noreply.github.com>
2022-01-15 17:02:50 +0900

    아이디 중복확인

--- LEE/MyApplication/app/src/main/java/com/example/knu_info/JoinActivity.java
+++ LEE/MyApplication/app/src/main/java/com/example/knu_info/JoinActivity.java
@@ -1,5 +1,6 @@
 package com.example.knu_info;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,9 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -17,10 +22,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class JoinActivity extends AppCompatActivity {
     String TAG = "JoinActivity";
@@ -32,6 +48,9 @@ public class JoinActivity extends AppCompatActivity {
     EditText etJPhone;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private AlertDialog dialog;
+    private boolean validate = false;
+    private DatabaseReference mDatabase;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
@@ -51,7 +70,44 @@ public class JoinActivity extends AppCompatActivity {
                 }
 
             }
+
         };
+        Button btnIDcheck = (Button) findViewById(R.id.btnID);
+        btnIDcheck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String id = etJid.getText().toString().trim();
+                //String pass = etPassword.getText().toString().trim();
+                    validate=true;
+                db.collection("users")
+                        .whereEqualTo("Id", id)
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                    if (document.getData().get("Id").equals(id)) {
+                                        Log.e(TAG, "onSuccess:아이디 중복");
+                                        Toast.makeText(getApplicationContext(), "이미 아이디가 존재합니다.", Toast.LENGTH_LONG).show();
+                                        etJid.setText(null);
+                                        return;
+                                    }
+                                }
+                                Toast.makeText(getApplicationContext(), "아이디 사용 가능", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "onFailure: 실패 ");
+                            }
+                        });
+
+                //LoginUser(id,pass);
+            }
+        });
+
+
+
         etJName = (EditText) findViewById(R.id.etJName);
         etJid = (EditText) findViewById(R.id.etJid);
         etJpass = (EditText) findViewById(R.id.etJpass);
@@ -60,6 +116,8 @@ public class JoinActivity extends AppCompatActivity {
         etJEmail = (EditText) findViewById(R.id.etJEmail);
 
         Button btnJjoin = (Button) findViewById(R.id.btnJjoin);
+        Button btnID=(Button)findViewById(R.id.btnID);
+
 
         //가입하기 버튼
         btnJjoin.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +128,19 @@ public class JoinActivity extends AppCompatActivity {
                 String stPass_check = etJpass_check.getText().toString();
                 String stPhone = etJPhone.getText().toString();
                 String stEmail = etJEmail.getText().toString();
+                if (!validate) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                    dialog = builder.setMessage("중복된 아이디가 있는지 확인하세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+                if (stName.equals("") || stId.equals("") || stPass.equals("")  || stPass_check.equals("") || stPhone.equals("")|| stEmail.equals("")  ) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                    dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
                 if (stPass.equals(stPass_check)) {
                     HashMap<String, String> user = new HashMap<>();
                     user.put("Name", stName);
