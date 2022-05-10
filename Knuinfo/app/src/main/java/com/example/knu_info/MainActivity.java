0b759d373fbb5581db810644b224b592e8c8e35d
LEE <dltmdals97x@naver.com>
2022-05-11 03:27:13 +0900

    로그아웃

--- Knuinfo/app/src/main/java/com/example/knu_info/MainActivity.java
+++ Knuinfo/app/src/main/java/com/example/knu_info/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.knu_info;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnchat;
     Button btntime;
+    Button btnlogout;
+    Button btnalarm;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,17 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
             btnchat=(Button)findViewById(R.id.btnchat);
             btntime=(Button)findViewById(R.id.btntime);
+            btnlogout =(Button)findViewById(R.id.btnlogout);
+            btnalarm=(Button)findViewById(R.id.btnalarm);
             String stId=getIntent().getStringExtra("id");
+            btnlogout.setOnClickListener((view)->{
+                Intent in = new Intent(MainActivity.this,LoginActivity.class) ;
+                in.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                Toast.makeText(getApplicationContext(),"로그아웃 되었습니다.",Toast.LENGTH_LONG).show();
+                startActivity(in);
+
+
+            });
 
             btnchat.setOnClickListener((view)->{
                 Intent in = new Intent(MainActivity.this,ChatActivity.class) ;
@@ -26,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
             });
             btntime.setOnClickListener((view)->{
                 Intent in = new Intent(MainActivity.this,TimetableActivity.class) ;
+                in.putExtra("id",stId);
+
+                startActivity(in);
+            });
+            btnalarm.setOnClickListener((view)->{
+                Intent in = new Intent(MainActivity.this,AlarmActivity.class) ;
                 in.putExtra("id",stId);
 
                 startActivity(in);
